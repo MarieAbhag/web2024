d5948e203fe02d4e2ac48de3257fb28271d0336c
Marie Louise <marieabhag282@gmail.com>
2024-07-01 08:14:30 +0200

    acer

---
+++ 240627/js/marie.js
@@ -0,0 +1,14 @@
+const x = document.querySelector("h1");  //bst3lm 3n tag
+const y= document.querySelector("#demo");  //bst3lm 3n  id
+const z = document.querySelector(".ausweiss");
+const a = document.getElementById("demo"); //htli elment el id bt3o demo
+
+
+function x (){}   //b3ln en feh function hna decleration
+let o = function ()
+o();
+
+
+let firstName = "Marie", lastName= "louise", age=31;
+
+console.log("firstsname"+ ""+ "lastName" + ""+ is +""+  + "age")
