a26628590ee85d441d0f57e70e99ea3e8dc68c05
Marie Louise <88967873+MarieAbhag@users.noreply.github.com>
2024-07-02 10:12:07 +0200

    asus
    a

---
+++ marie0207/js/mehrereJson.js
@@ -0,0 +1,33 @@
+// __________________________mit variabler json + id _____________________
+function ladeBilder(json,id) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if( xhr.readyState === 4 && xhr.status === 200) {
+            const obj = JSON.parse(xhr.responseText);
+            const arr = obj.ereignisse;
+            const frag = document.querySelector(id);
+
+            // console.log(arr);
+            // console.log(frag);
+            for (let i of arr) {
+                let divi = document.createElement('div');
+                let bild = document.createElement('img');
+                let span = document.createElement('span');
+
+                divi.append(span,bild);
+                frag.append(divi);
+            }
+         }
+    }
+
+    xhr.open('GET','extern/'+json, true);
+    xhr.send(null);
+}
+
+document.querySelector('#eins').addEventListener('click', function() {
+    ladeBilder('woche.json','#contant'); 
+});
+document.querySelector('#zwei').addEventListener('click', function() {
+    ladeBilder('woche2.json','#contant2'); 
+});
+// loadText();
