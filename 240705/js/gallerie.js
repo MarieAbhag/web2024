6c6025b03ef5b406c3793031c9b06c02a520be9a
Marie Louise <88967873+MarieAbhag@users.noreply.github.com>
2024-07-05 12:07:57 +0200

    asus

--- 240705/js/gallerie.js
+++ 240705/js/gallerie.js
@@ -27,4 +27,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-} 
+}
+//  ------------------------------------------------------
+var i = 0;
+var images = [];
+var time = 1000;
+
+images[0] = "img/image1.png";
+images[1] = "img/image2.png";
+images[2] = "img/image3.png";
+
+function changeImg(){
+document.slide.src = images[i];
+if (i < images.length -  1) {
+ i++
+}else {
+  i = 0;
+}
+  setTimeout("changeImg()", time)
+}
+window.onload = changeImg;
