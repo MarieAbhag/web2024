24e2d446c4e6228a13c1dbd55a4aaec7bebdd5cf
Marie Louise <88967873+MarieAbhag@users.noreply.github.com>
2024-07-10 08:41:14 +0200

    asus

--- 240709/js/fetchZweiUrls.js
+++ 240709/js/fetchZweiUrls.js
@@ -2,35 +2,53 @@ const url = 'extern/woche.json';
 const url2 = 'extern/woche2.json';
 
 //request durchführen mit fetch(ist ein promise)
+////perform request with fetch (is a promise)
 //und mit .then
+
+//fetch (url)
+//.then((){})
+//(function(response){if else})
+
+
+
+
+
 fetch(url)
 
-.then(function(response) {
+.then(function(response) {                                  //fetch (url)
+
     console.log(response.ok);   //true
     if(response.ok) {
         return response.json();
     }else{
         //eigene fehlermeldung
-        throw new Error('ein fehler ist aufgetreten');
+        throw new Error('ein fehler ist aufgetreten');     //occurred
     }
 })
-
+//.then(()=>{})
 .then((data) => {
-    console.log('url1 '+data);      //url1 [object Object]
+    console.log('url1 '+data);                      //url1 [object Object]
 })
 
 .then(
-    fetch(url2).then(function(response) {
+    fetch(url2)
+    .then(function(response) {
         return response.json();
-    }).then(function(data) {
-        console.log('url2 '+data);  //url2 [object Object]
+    })
+    .then(function(data) {
+        console.log('url2 '+data);                  //url2 [object Object]
         for( const freu of data.ereignisse ) {      //zugriff auf das array ereignisse in data
-            console.log(freu);
+            console.log(freu);                      //Object { location: "heute", map: "img/vier.png" } 
         }
     })
 )
-
+//const url = 'extern/woch.json';                   //delete l7rf e fi woche
 .catch((error,response) => {
     console.error(error);
-    if(!response) { console.log('keine antwort'); }
+    if(!response) { console.log('keine antwort'); } 
+
 })
+//Error: ein fehler ist aufgetreten
+//     <anonymous> http://127.0.0.1:5500/240709/js/fetchZweiUrls.js:25
+//     promise callback* http://127.0.0.1:5500/240709/js/fetchZweiUrls.js:18
+// 
